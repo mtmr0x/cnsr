3435c13a5ce8cd9c10e5020c4014871359790161
Matheus Marsiglio <matmarsiglio@gmail.com>
2017-07-22 22:00:39 -0300

    refactor: make one higher order function call with conditional in it

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 function masker(obj, maskable = []) {
-  const newObj = obj;
   const objKeys = Object.keys(obj);
 
   // iterate maskable objects for replacing
   // characters for * in case object key exists
   // in maskable array
-  objKeys
-    .filter(o => maskable.includes(o) && (typeof obj[o] === 'number' || typeof obj[o] === 'string'))
-    .map(o => {
-      newObj[o] = newObj[o].toString().replace(/./g, '*');
-      return false
-    });
+  objKeys.map(o => {
+    if (['string', 'number'].includes(typeof obj[o]) && maskable.includes(o))
+      obj[o] = obj[o].toString().replace(/./g, '*');
+    else if (typeof obj[o] === 'object')
+      masker(obj[o], maskable)
+    return false
+  });
 
-  // filter objects in obj for recursively call
-  // masker function for mask all levels data
-  objKeys
-    .filter(o => typeof obj[o] === 'object')
-    .map(o => masker(newObj[o], maskable));
-
-  return newObj;
+  return obj;
 }
 
 function cnsr(obj, mask) {
@@ -29,13 +23,10 @@ function cnsr(obj, mask) {
   if (typeof mask !== 'object' || !mask.length) {
     throw new TypeError('cnsr second parameter is a required Array of strings for matching maskable data in your object. For more information see docs and usage at https://github.com/mtmr0x/cnsr');
   }
-  // Assign a new object to prevent
-  // side effects in your original data
-  const j = JSON.stringify(obj);
 
   // recreate a new object based in JSON
   // for avoiding side effects
-  return masker(JSON.parse(j), mask);
+  return masker(JSON.parse(JSON.stringify(obj)), mask);
 }
 
 module.exports = cnsr;
